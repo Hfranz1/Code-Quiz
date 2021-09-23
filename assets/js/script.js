e83463b6ad38bee270475d7bbf5dd5883c87180b
Hfranz1 <87962035+Hfranz1@users.noreply.github.com>
2021-09-22 20:38:14 -0600

    var js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
+var startQuizButton = document.getElementById("startQuizButton");
+var submitButton = document.getElementById("submitButton");
+var goBackButton = document.getElementById("goBackButton");
+var clearHighScores =document.getElementById("ClearHighscore");
+
+var mainPage = document.getElementById("mainPage");
+var questionContainer = document.getElementById("questionContainer");
+var initials = document.getElementById("initials")
+
+
+
+var timerImput = document.getElementById("countdown");
+var timeLeft = 60;
+var userScore = 0;
+
+
+var finalScorePage = document.getElementById("finalScore");
+var highScorePage = document.getElementById("highscores");
+var scoreInout = document.getElementById("score");
+var highscoreDiv = document.getElementById("highscoreDiv");
 
